87f7fb11946e82d1c022571a1a64979ba1ac3a33
nohanaassis-ui <nohana.assis@edu.senai.br>
2026-04-08 14:20:04 -0300

    feat exercicio 1 while

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class exo1{}
+void main() {
+      int num = 1;
+
+      //while (enquanto for verdadeiro ) {
+      //ele roda se for verdadeiro
+
+      while (num < 11 ){
+            IO.println(num);
+            num++;
+
+      }
+}
